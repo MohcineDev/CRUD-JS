5ff3483d6a4673967d3444741cd73cd6422edc94
MohcineDev <mouhcinedev@gmail.com>
2022-08-12 23:46:02 +0100

    get inputs

---
+++ script.js
@@ -0,0 +1,30 @@
+const Title = document.getElementById('title')
+const Price = document.getElementById('price')
+const Taxes = document.getElementById('taxes')
+const Ads = document.getElementById('ads')
+const Discount = document.getElementById('discount')
+const Total = document.getElementById('total')
+const Count = document.getElementById('count')
+const Category = document.getElementById('category')
+const Submit = document.getElementById('submit')
+ 
+//get total
+
+
+//create prodact
+
+//save data  in local storage
+
+//clear inputs
+
+//read data
+
+//count
+
+//delete
+
+//update
+
+// search
+
+//clean data
