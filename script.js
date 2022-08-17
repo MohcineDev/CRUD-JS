e00fdef7a4e8732ffaf70d47c59f6f24ef88ad41
MohcineDev <mouhcinedev@gmail.com>
2022-08-17 23:43:36 +0100

    add products to local storage

--- script.js
+++ script.js
@@ -8,16 +8,73 @@ const Count = document.getElementById('count')
 const Category = document.getElementById('category')
 const Submit = document.getElementById('submit')
 
+const Tbody = document.querySelector('table tbody')
+
+let myProducts = []
 //get total
+let i = 0
+
+const checkInput = (value) => {
+    return isNaN(parseInt(value)) ? 0 : parseInt(value)
+}
+
 const getTotal = () => {
-    if (parseInt(Price.value) !== '') {
-        let result = (+Price.value + +Taxes.value + +Ads.value) - +Discount.value
+
+    if (checkInput(Price.value) != '') {
+        let result = (checkInput(Price.value) + checkInput(Taxes.value) + checkInput(Ads.value)) - checkInput(Discount.value)
         Total.textContent = result
- 
+        Total.style.backgroundColor = 'rgb(204, 253, 204)'
+
+
+    }
+    else {
+        Total.style.backgroundColor = '#f77575'
+        Total.textContent = '--'
     }
 }
 
-//create prodact
+//create product
+
+//////// create and append td
+const createTd = (value) => {
+    let td = document.createElement('td')
+    td.textContent = value
+    return td
+}
+Submit.addEventListener('click', () => {
+    let product = {
+        id: i,
+        title: Title.value,
+        price: Price.value,
+        tax: Taxes.value,
+        ads: Ads.value,
+        discount: Discount.value,
+        total: Total.textContent,
+        count: Count.value,
+        category: Category.value
+    }
+
+    myProducts.push(product)
+    i++
+
+    localStorage.setItem('products', JSON.stringify(myProducts))
+
+    let tr = document.createElement('tr')
+
+    let id = createTd(product.id)
+    let title = createTd(product.title)
+    let price = createTd(product.price)
+    let tax = createTd(product.tax)
+    let ads = createTd(product.ads)
+    let discount = createTd(product.discount)
+    let total = createTd(product.total)
+    let count = createTd(product.count)
+    let category = createTd(product.category)
+
+    tr.append(id,title,  price,tax, ads, discount, total, count, category)
+
+    Tbody.appendChild(tr)
+})
 
 //save data  in local storage
 
